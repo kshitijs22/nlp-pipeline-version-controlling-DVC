77cc8406b657229a55e1dd2dedacf02475257dd8
Kshitij <ksinfy22@gmail.com>
2024-10-25 15:33:32 +0530

    src folder, dvc.yaml, params.yaml updated in cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Creating versions of data and code using dvc and git',
+    author='kshitij',
+    license='',
+)
